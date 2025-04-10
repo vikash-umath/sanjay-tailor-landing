9c08259f3bad4d070857649d8a7a4ff33ebd2d13
Vikash Maheshwari <vikasmaheshwari6267@gmail.com>
2025-04-10 13:43:08 +0530

    update code sitemap and seo

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 8080,
+    port: 3000,
   },
   plugins: [
     react(),
